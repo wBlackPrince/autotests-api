fd82f91acbf96aeaf37476a97b39da671667e4f5
wBlackPrince <masteryoda021@gmail.com>
2025-06-08 17:50:51 +0300

    first grpc server-client implementation

---
+++ grpc_client.py
@@ -0,0 +1,10 @@
+import grpc
+import user_service_pb2_grpc
+import user_service_pb2
+
+channel = grpc.insecure_channel('localhost:50051')
+
+stub = user_service_pb2_grpc.UserServiceStub(channel)
+
+response = stub.GetUser(user_service_pb2.GetUserRequest(username="Alice"))
+print(response)
--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-print('Hi')
+import json
+
+with open("json_example.json") as j:
+    data = json.load(j)
+
+    for i in data.keys():
+        print(i, data[i])
+
 
 if __name__ == "__main__":
     print("PyCharm")
