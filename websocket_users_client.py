63a5f22b1e3d8ca5ff8f984489439dd0bbed9562
wBlackPrince <masteryoda021@gmail.com>
2025-06-09 17:59:21 +0300

    5.6. Практика работы с WebSocket протоколом

--- websocket_users_client.py
+++ websocket_users_client.py
@@ -4,13 +4,10 @@ import websockets
 async def client():
     uri = "ws://localhost:8765"
     async with websockets.connect(uri) as websocket:
-        message = "Привет, сервер"
-        print(f"Отправка {message}...")
-        await websocket.send(message)
+        await websocket.send("Привет, сервер!")
 
         for i in range(5):
             response = await websocket.recv()
-            print(f"Ответ от  {response}")
-
+            print(response)
 
 asyncio.run(client())
