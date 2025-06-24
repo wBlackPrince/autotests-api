fc2f6c71a8de61632266c1b7db3e1523f41f664c
wBlackPrince <masteryoda021@gmail.com>
2025-06-24 16:16:19 +0300

    8.4 Pytest skip, skipif, xfail

---
+++ tests/test_pytest_skip.py
@@ -0,0 +1,5 @@
+import pytest
+
+@pytest.mark.skip(reason = "Фича еще в разработке")
+def test_feature_in_development():
+    ...
---
+++ tests/test_pytest_xfail.py
@@ -0,0 +1,13 @@
+import pytest
+
+@pytest.mark.xfail(reason="Найден баг в приложении из-за которого тест падает с ошибкой")
+def test_with_bug():
+    assert 1 == 2
+
+@pytest.mark.xfail(reason="Баг уже исправлен, но на тесте все еще висит маркировка xfail")
+def test_without_bug():
+    ...
+
+@pytest.mark.xfail(reason="Внешний сервис временно недоступен")
+def test_external_services_is_unavailable():
+    ...
